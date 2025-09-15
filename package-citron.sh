012a8f9741185879718a16f2231cb71d53899f12
CollectingW <ward9128@gmail.com>
2025-09-14 21:40:18 -0400

    rename citron-appimage.sh to package-citron.sh

--- package-citron.sh
+++ package-citron.sh
@@ -1,8 +1,8 @@
-#!/bin/sh 
+#!/bin/sh
 
 set -ex
 
-ARCH="$(uname -m)"
+ARCH="${ARCH:-$(uname -m)}"
 if [ "$1" = 'v3' ] && [ "$ARCH" = 'x86_64' ]; then
 	ARCH="${ARCH}_v3"
 fi
@@ -10,40 +10,33 @@ VERSION="$(cat ~/version)"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 
-export ADD_HOOKS="self-updater.bg.hook"
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export OUTNAME=Citron-"$VERSION"-anylinux-"$ARCH".AppImage
 export DESKTOP=/usr/share/applications/org.citron_emu.citron.desktop
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.citron_emu.citron.svg
-export DEPLOY_OPENGL=1 
-export DEPLOY_VULKAN=1 
+export DEPLOY_OPENGL=1
+export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
-# ADD LIBRARIES
 wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 ./quick-sharun /usr/bin/citron* /usr/lib/libgamemode.so* /usr/lib/libpulse.so*
 
-# TODO: remove me once we can build citron again without qt6-webengine nonsense
-cp -rv /usr/share/qt6/resources    ./AppDir/shared/lib/qt6
+cp -rv /usr/share/qt6/resources ./AppDir/shared/lib/qt6
 cp -rv /usr/share/qt6/translations ./AppDir/shared/lib/qt6
 
 if [ "$DEVEL" = 'true' ]; then
 	sed -i 's|Name=citron|Name=citron nightly|' ./AppDir/*.desktop
-	UPINFO="$(echo "$UPINFO" | sed 's|latest|nightly|')"
 fi
 
-# allow the host vulkan to be used for aarch64 given the sad situation
 if [ "$ARCH" = 'aarch64' ]; then
 	echo 'SHARUN_ALLOW_SYS_VKICD=1' > ./AppDir/.env
 fi
 
-# MAKE APPIMAGE WITH URUNTIME
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 ./uruntime2appimage
 
 mkdir -p ./dist
 mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+mv -v ~/version ./dist
 echo "All done!"
